92b43c37260be4bc138b858f8539ec37d6d0e364
Viktor <viktoryanev222@gmail.com>
2025-05-01 14:44:58 +0300

    Create and Delete actions

---
+++ Private/CRUD/create.php
@@ -0,0 +1,85 @@
+<?php
+function add_author(string $first_name, string $last_name): void
+{
+    include("../Misc/config.php");
+    $sql = "INSERT INTO authors(FirstName, LastName) VALUES('$first_name', '$last_name')";
+    $result = mysqli_query($dbConn, $sql);
+    if (!$result) {
+        die("Could not add Author " . mysqli_error($dbConn));
+    }
+}
+
+function add_genre(string $genre_name): void
+{
+    include("../Misc/config.php");
+    $sql = "INSERT INTO genres(GenreName) VALUES('$genre_name')";
+    $result = mysqli_query($dbConn, $sql);
+    if (!$result) {
+        die("Could not add Genre " . mysqli_error($dbConn));
+    }
+}
+
+function add_position(string $position_name): void
+{
+    include("../Misc/config.php");
+    $sql = "INSERT INTO positions(Position) VALUES('$position_name')";
+    $result = mysqli_query($dbConn, $sql);
+    if (!$result) {
+        die("Could not add Position " . mysqli_error($dbConn));
+    }
+}
+
+function add_client(string $first_name, string $last_name, string $email, string $phone_number): void
+{
+    include("../Misc/config.php");
+    $sql = "INSERT INTO clients(FirstName, LastName, Email, PhoneNumber) VALUES('$first_name', '$last_name', '$email', '$phone_number')";
+    $result = mysqli_query($dbConn, $sql);
+    if (!$result) {
+        die("Could not add Client " . mysqli_error($dbConn));
+    }
+}
+
+// User will be able to select position from a drop down menu which will then be processed into position_id.
+function add_employee(string $first_name, string $last_name, string $email, string $phone_number, int $position_id): void
+{
+    include("../Misc/config.php");
+    $sql = "INSERT INTO clients(FirstName, LastName, Email, PhoneNumber, PositionId) VALUES('$first_name', '$last_name', '$email', '$phone_number', $position_id)";
+    $result = mysqli_query($dbConn, $sql);
+    if (!$result) {
+        die("Could not add Employee " . mysqli_error($dbConn));
+    }
+}
+
+function add_book_authors(int $book_id, int $author_id)
+{
+    include("../Misc/config.php");
+    $sql = "INSERT INTO book_authors(BookId, AuthorId) VALUES($book_id, $author_id)";
+    $result = mysqli_query($dbConn, $sql);
+    if (!$result) {
+        die("Could not add BookAuthor " . mysqli_error($dbConn));
+    }
+}
+
+function add_book(string $title, int $year, string $publisher, int $genre_id, int $times_borrowed, array $authors): void
+{
+    include("../Misc/config.php");
+    $sql = "INSERT INTO books(Title, PublishYear, Publisher, GenreId, TimesBorrowed) VALUES('$title', $year, '$publisher', $genre_id, $times_borrowed)";
+    $result = mysqli_query($dbConn, $sql);
+    if (!$result) {
+        die("Could not add Employee " . mysqli_error($dbConn));
+    }
+    // add bookAuthors
+    foreach ($authors as $author_id) {
+        add_book_authors(mysqli_insert_id($dbConn), $author_id);
+    }
+}
+
+function add_borrow(int $book_id, int $client_id, int $employee_id)
+{
+    include("../Misc/config.php");
+    $sql = "INSERT INTO borrows(BookId, ClientId, EmployeeId, ReturnDate) VALUES($book_id, $client_id, $employee_id, CURDATE() + 14)";
+    $result = mysqli_query($dbConn, $sql);
+    if (!$result) {
+        die("Could not Borrow " . mysqli_error($dbConn));
+    }
+}
---
+++ Private/CRUD/delete.php
@@ -0,0 +1,77 @@
+<?php
+function delete_author(int $author_id)
+{
+    include("../Misc/config.php");
+    $sql = "DELETE FROM authors WHERE AuthorId=$author_id";
+    $result = mysqli_query($dbConn, $sql);
+    if (!$result) {
+        die("Could not delete author " . mysqli_error($dbConn));
+    }
+}
+
+function delete_genre(int $genre_id)
+{
+    include("../Misc/config.php");
+    $sql = "DELETE FROM genres WHERE GenreId=$genre_id";
+    $result = mysqli_query($dbConn, $sql);
+    if (!$result) {
+        die("Could not delete genre " . mysqli_error($dbConn));
+    }
+}
+function delete_position(int $position_id)
+{
+    include("../Misc/config.php");
+    $sql = "DELETE FROM positions WHERE PositionId=$position_id";
+    $result = mysqli_query($dbConn, $sql);
+    if (!$result) {
+        die("Could not delete position " . mysqli_error($dbConn));
+    }
+}
+function delete_borrow(int $borrow_id)
+{
+    include("../Misc/config.php");
+    $sql = "DELETE FROM borrows WHERE BorrowId=$borrow_id";
+    $result = mysqli_query($dbConn, $sql);
+    if (!$result) {
+        die("Could not delete borrow " . mysqli_error($dbConn));
+    }
+}
+function delete_client(int $client_id)
+{
+    include("../Misc/config.php");
+    $sql = "DELETE FROM clients WHERE ClientId=$client_id";
+    $result = mysqli_query($dbConn, $sql);
+    if (!$result) {
+        die("Could not delete client " . mysqli_error($dbConn));
+    }
+}
+function delete_employee(int $employee_id)
+{
+    include("../Misc/config.php");
+    $sql = "DELETE FROM employees WHERE EmployeeId=$employee_id";
+    $result = mysqli_query($dbConn, $sql);
+    if (!$result) {
+        die("Could not delete employee " . mysqli_error($dbConn));
+    }
+}
+function delete_book_authors(int $book_id, int $author_id)
+{
+    include("../Misc/config.php");
+    $sql = "DELETE FROM book_authors WHERE BookId=$book_id AND AuthorId=$author_id";
+    $result = mysqli_query($dbConn, $sql);
+    if (!$result) {
+        die("Could not delete author " . mysqli_error($dbConn));
+    }
+}
+
+function delete_book(int $book_id)
+{
+    include("../Misc/config.php");
+    $sql = "DELETE FROM books WHERE BookId=$book_id";
+    $result = mysqli_query($dbConn, $sql);
+    if (!$result) {
+        die("Could not delete book " . mysqli_error($dbConn));
+    }
+    while ($result = mysqli_query($dbConn, "DELETE FROM book_authors WHERE BookId=$book_id"))
+        ;
+}
